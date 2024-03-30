7459c3ecff0ab994676025dc306fd458ae408435
Ahmed Helmy <42387111+AhmddHelmy@users.noreply.github.com>
2024-03-30 16:02:42 +0200

    finish task

---
+++ src/components/post-card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card m-2" style="width: 18rem;">
+        <img :src="'https://picsum.photos/600/300/?image=' + post.id" class="card-img-top" alt="Post Image">
+        <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">User: {{ user.name }}</p>
+            <p class="card-text">Phone: {{ user.phone }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    props: ['post'],
+    data() {
+        return {
+            user: {}
+        }
+    },
+    mounted() {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${this.post.userId}`)
+            .then(response => {
+                this.user = response.data
+            })
+            .catch(error => {
+                console.error('Error fetching user details:', error)
+            })
+    }
+}
+</script>
---
+++ src/components/post-list.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <post-card v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+</template>
+
+<script>
+import { Axios } from 'axios'
+import PostCard from './PostCard.vue'
+
+export default {
+    components: {
+        PostCard
+    },
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.posts = response.data
+            })
+            .catch(error => {
+                console.error('Error fetching posts:', error)
+            })
+    }
+}
+</script>
